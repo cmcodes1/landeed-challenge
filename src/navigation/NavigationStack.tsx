0f6d6fd64d3460e2bc1e65f629e0ff9e32d63210
C M Pandey <chandramohan.pandey2015@gmail.com>
2024-03-17 13:17:31 +0530

    ➕ navigation setup

---
+++ src/navigation/NavigationStack.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import PersonalDetails from '../screens/PersonalDetails';
+import ProfessionalDetails from '../screens/ProfessionalDetails';
+
+const Stack = createNativeStackNavigator();
+
+export default function NavigationStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="PersonalDetails"
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen name="PersonalDetails" component={PersonalDetails} />
+      <Stack.Screen
+        name="ProfessionalDetails"
+        component={ProfessionalDetails}
+      />
+    </Stack.Navigator>
+  );
+}
